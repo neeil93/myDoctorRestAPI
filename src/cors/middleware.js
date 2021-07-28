fa38a789c30be6dbee2a3e05044061a5ea6a2f85
Sunil <chandrasunil93@gmail.com>
2021-07-28 18:39:32 +0530

    single user route added

--- src/cors/middleware.js
+++ src/cors/middleware.js
@@ -3,19 +3,24 @@ import { APP_SECRET } from "../utils/authUtils.js";
 import { errorResponse, successResponse } from "./responseHandler.js";
 
 export function checkToken(req, res, next) {
-  let token = (req.headers['authorization']).replace('Bearer ', '');
+  let token = req.headers["authorization"].replace("Bearer ", "");
   if (token) {
-    jwt.verify(token, APP_SECRET, {
-      ignoreExpiration: true
-    }, (err, decoded) => {
-      if (err) {
-        return res.status(414).send(errorResponse(414));
-      } else {
+    jwt.verify(
+      token,
+      APP_SECRET,
+      {
+        ignoreExpiration: true,
+      },
+      (err, decoded) => {
+        if (err) {
+          return res.status(414).send(errorResponse(414));
+        } else {
           decoded.isAdminUser = false;
-        req.decoded = decoded;
-        next();
+          req.decoded = decoded;
+          next();
+        }
       }
-    });
+    );
   } else {
     return res.status(415).send(errorResponse(415));
   }
